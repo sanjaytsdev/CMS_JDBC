d861e1846ca97cc0b7232d6b681165ae1b81c77d
Sanjay TS <sanjaytsdev@gmail.com>
2024-08-22 14:47:10 +0530

    Contact Management System using JDBC & SWING

---
+++ src/Domain/DAO/baseDao.java
@@ -0,0 +1,6 @@
+package Domain.DAO;
+
+public class baseDao {
+    public static final String DRIVER_NAME = "org.sqlite.JDBC";
+    public static final String URL = "jdbc:sqlite:D:\\intellij\\workspace\\Contact management\\src\\Domain\\DAO\\contacts.db";
+}
